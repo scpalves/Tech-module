af326a2482cb52c0f2dd772f3c289c8340313a65
Aleksandur Gyuzelov <aguzelov@outlook.com>
2017-10-07 18:43:15 +0300

    Add List - Exercises

--- Programming Fundamentals/16. List - Exercises/Lists - Exercises/LongestIncreasingSubsequence/LongestIncreasingSubsequence.cs
+++ Programming Fundamentals/16. List - Exercises/Lists - Exercises/LongestIncreasingSubsequence/LongestIncreasingSubsequence.cs
@@ -4,9 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _ListMethods
+namespace LongestIncreasingSubsequence
 {
-   public class ListMethods
+    class LongestIncreasingSubsequence
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = Console.ReadLine()
+                                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                .Select(i => int.Parse(i))
+                                .ToArray();
+
+            int[] length = new int[sequence.Length];
+            int[] prev = new int[sequence.Length];
+
+            int maxLength = 0;
+            int lastIndex = -1;
+            
+            for(int i = 0; i <sequence.Length; i++)
+            {
+                length[i] = 1;
+                prev[i] = -1;
+
+                for(int j = 0; j < i; j++)
+                {
+                    if(sequence[j] < sequence[i] && length[j] >= length[i])
+                    {
+                        length[i] = 1 + length[j];
+                        prev[i] = j;
+                    }
+                }
+
+                if(length[i] > maxLength)
+                {
+                    maxLength = length[i];
+                    lastIndex = i;
+                }
+            }
+
+            List<int> longestSequence = new List<int>();
+
+            for(int i = 0; i < maxLength; i++)
+            {
+                longestSequence.Add(sequence[lastIndex]);
+                lastIndex = prev[lastIndex];
+            }
+
+            longestSequence.Reverse();
+
+            Console.WriteLine(string.Join(" ", longestSequence));
+        }
+    }
+    
+
+    public class ListMethods
     {
         public static bool CorrectIndex<T>(List<T> list, int index)
         {
